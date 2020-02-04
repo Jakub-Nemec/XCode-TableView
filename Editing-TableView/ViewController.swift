3d6c2465e310cbf4893f4e5c6cd326937e6cd076
Jakub Němec <jakub.k.nemec@gmail.com>
2020-02-04 07:47:58 +0100

    Initial Commit

---
+++ Editing-TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Editing-TableView
+//
+//  Created by Jakub Němec on 04/02/2020.
+//  Copyright © 2020 Jakub Němec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
