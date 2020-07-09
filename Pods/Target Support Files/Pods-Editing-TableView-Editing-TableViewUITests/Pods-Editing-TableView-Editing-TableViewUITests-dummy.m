9f0a42217b104dd42a32e521e02d40f80791ccae
Jakub Němec <Jakub.K.Nmec@gmail.com>
2020-07-09 12:42:23 +0200

    Podfile

---
+++ Pods/Target Support Files/Pods-Editing-TableView-Editing-TableViewUITests/Pods-Editing-TableView-Editing-TableViewUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Editing_TableView_Editing_TableViewUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Editing_TableView_Editing_TableViewUITests
+@end
